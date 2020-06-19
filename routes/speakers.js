440d6bb44200c33794baeb4ce62a294f13da704a
Paulo Belo <pjbelo@gmail.com>
2020-06-19 16:58:07 +0100

    routes functions

---
+++ routes/speakers.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const router = express.Router();
+
+module.exports = () => {
+    router.get('/', (request, response) => {
+        return response.send('Speakers List');
+    });
+
+    router.get('/:shortname', (request, response) => {
+        return response.send(`Detail page of ${request.params.shortname}`);
+    });
+
+    return router;
+}
