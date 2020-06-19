c77e651f512c1988dc651f266f84f28fdf7cfda4
Paulo Belo <pjbelo@gmail.com>
2020-06-19 18:07:15 +0100

    remove visit counter

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,6 @@ const router = express.Router();
 
 module.exports = params => {
     router.get('/', (request, response) => {
-
-        if(!request.session.visitcount){
-            request.session.visitcount = 0;
-        }
-        request.session.visitcount +=1;
-        console.log(`Number of visits: ${request.session.visitcount}`);
-
         response.render('pages/index', { pageTitle: 'Welcome'});
     });
 
