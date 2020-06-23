b60e23a7aeede1eb15c72ad4efd14729d2dc45cd
Paulo Belo <pjbelo@gmail.com>
2020-06-23 13:08:33 +0100

    template variables

--- server.js
+++ server.js
@@ -17,24 +17,39 @@ const port = 3000;
 // trust reverse proxy servers like nginx
 app.set('trust proxy', 1);
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['dEd4dew342ED', 'DEDdgt7646JYg'],
-}))
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['dEd4dew342ED', 'DEDdgt7646JYg'],
+  })
+);
 
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '/views'));
 
+app.locals.siteName = 'ROUX Meetups';
+
 app.use(express.static(path.join(__dirname, './static')));
 
+app.use(async (request, response, next) => {
+  try {
+    const names = await speakersService.getNames();
+    response.locals.speakerNames = names;
+    console.log(response.locals);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
 app.use(
-  '/', 
+  '/',
   routes({
     feedbackService,
     speakersService,
   })
-  );
+);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
