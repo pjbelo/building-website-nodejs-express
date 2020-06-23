d11846a476b1e2f12b454c32b8472ea73f859921
Paulo Belo <pjbelo@gmail.com>
2020-06-23 12:37:26 +0100

    site-wide layout

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ const feedbackRoute = require('./feedback');
 
 const router = express.Router();
 
-module.exports = params => {
-    router.get('/', (request, response) => {
-        response.render('pages/index', { pageTitle: 'Welcome'});
-    });
+module.exports = (params) => {
+  router.get('/', (request, response) => {
+    response.render('layout', { pageTitle: 'Welcome', template: 'index' });
+  });
 
-    router.use('/speakers', speakersRoute(params));
-    router.use('/feedback', feedbackRoute(params));
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
 
-    return router;
-}
+  return router;
+};
