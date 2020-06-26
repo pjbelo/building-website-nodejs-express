d9ff043493655e63b84e6850976002edb9e94301
Paulo Belo <pjbelo@gmail.com>
2020-06-26 21:48:35 +0100

    4-7-Challenge-Partials and lists

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ const feedbackRoute = require('./feedback');
 
 const router = express.Router();
 
-module.exports = params => {
+module.exports = (params) => {
   const { speakersService } = params;
 
   router.get('/', async (request, response) => {
+    const artwork = await speakersService.getAllArtwork();
     const topSpeakers = await speakersService.getList();
-    response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers });
+    response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers, artwork });
   });
 
   router.use('/speakers', speakersRoute(params));
--- routes/speakers.js
+++ routes/speakers.js
@@ -1,21 +1,26 @@
 const express = require('express');
 
-
 const router = express.Router();
 
-module.exports = params => {
-    const { speakersService } = params;
+module.exports = (params) => {
+  const { speakersService } = params;
 
-    router.get('/', async (request, response) => {
-        const speakers = await speakersService.getList();
-        response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
-      });
+  router.get('/', async (request, response) => {
+    const speakers = await speakersService.getList();
+    const artwork = await speakersService.getAllArtwork();
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
+  });
 
-    router.get('/:shortname', async (request, response) => {
-        const speaker = await speakersService.getSpeaker(request.params.shortname);
-        console.log(speaker);
-        response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker });
+  router.get('/:shortname', async (request, response) => {
+    const speaker = await speakersService.getSpeaker(request.params.shortname);
+    const artwork = await speakersService.getArtworkForSpeaker(speaker.shortname);
+    response.render('layout', {
+      pageTitle: 'Speakers',
+      template: 'speakers-detail',
+      speaker,
+      artwork,
     });
+  });
 
-    return router;
-}
+  return router;
+};
