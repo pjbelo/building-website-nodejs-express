31790f78b91e849922d64ef4ccd3b0e060fa3078
Paulo Belo <pjbelo@gmail.com>
2020-06-26 17:26:00 +0100

    4-5-Creating a list page

--- routes/speakers.js
+++ routes/speakers.js
@@ -8,8 +8,8 @@ module.exports = params => {
 
     router.get('/', async (request, response) => {
         const speakers = await speakersService.getList();
-        return response.json(speakers);
-    });
+        response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
+      });
 
     router.get('/:shortname', (request, response) => {
         return response.send(`Detail page of ${request.params.shortname}`);
